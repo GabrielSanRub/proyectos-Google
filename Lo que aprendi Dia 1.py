72af1b3944be3ac4b0e2db911264b04ff59e4ee2
Gabriel <gabrielsa.rubiano@gmail.com>
2026-05-26 20:17:30 -0500

    Practica Dia 1

---
+++ Lo que aprendi Dia 1.py
@@ -0,0 +1,4 @@
+print ("Hola")
+print ("Soy Gabriel y la fecha de hoy es 26/05/2026")
+print ("Estoy comenzando a aprender y la verdad me esta gustando bastante y se que sera un largo camino")
+#Es lo primero asi que no esperes bastante yo del futuro
